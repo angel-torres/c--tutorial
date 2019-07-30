ebc6264371e4d2d662077f3f9a05dc64a91a4f76
angel-torres <alas4224@gmail.com>
2019-07-29 23:39:25 -0500

    bring in boilerplate project from freeCodeCamp youtube tutorial

---
+++ freeCodeCampTutorial/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace freeCodeCampTutorial
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ freeCodeCampTutorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace freeCodeCampTutorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.ReadLine();
+        }
+    }
+}
